3b9a14194a09b1b443dbdd9ccd1e091c9f1cbe2a
EC2 Default User <ec2-user@ip-172-31-43-93.ap-northeast-1.compute.internal>
2023-12-23 12:19:38 +0000

    Bookers2_バリデーション設定、sign up-key設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :books, dependent: :destroy
   # バリデーションで名前設定
-   validates :name, length: { minimum: 2 }, presence: true
-  
+   validates :name, length: { minimum: 2, maximum: 20 }
+  validates :introduction, length: { maximum: 50 }
+
   #プロフィール画像の設定
   has_one_attached :profile_image
-  
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
