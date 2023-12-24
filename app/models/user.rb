198c3bc865a65b4ea27a13e039b9c2927d5579cf
EC2 Default User <ec2-user@ip-172-31-43-93.ap-northeast-1.compute.internal>
2023-12-24 14:11:39 +0000

    Bookers2_RSpecテスト1

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   # バリデーションで名前設定
-   validates :name, length: { minimum: 2, maximum: 20 }
+   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
   #プロフィール画像の設定
